c2b8199029d7ee2d198cdf91701c9c6d677e0c6c
Paulo Henrique <phrocha8530@gmail.com>
2025-11-26 00:19:46 -0300

    26-11-25--START

---
+++ THE LEGEND OF CLASH/jogo.s
@@ -0,0 +1,67 @@
+.data
+.include "menu.s"
+
+.text
+
+SETUP:		la a0,char
+			li a1,0
+			li a2,0
+			li a3,0
+			call PRINT
+
+#
+#	a0 = endereço imagem
+#	a1 = x
+#	a2 = y
+#	a3 = frame (0 ou 1)
+##
+#
+#	t0 = endereço do bitmap display
+#	t1 = endereço da imagem
+#	t2 = contador de linha
+#	t3 = contador de coluna
+#	t4 = largura
+#	t5 = altura
+
+
+PRINT:		li t0,0xFF0
+			add t0,t0,a3
+			slli t0,t0,20
+			
+			add t0,t0,a1
+			
+			li t1,320
+			mul t1,t1,a2
+			add t0,t0,t1
+			
+			addi t1,a0,8 # tem que pular 8 pq o menu.s tem tamanho "word" ou seja 4 + 4 = 8
+			
+			mv t2,zero
+			mv t3,zero
+			
+			lw t4,0(a0)
+			lw t5,5(a0)
+		
+PRINT_LINHA:	lw t6,0(t1)
+			sw t6,0(t0)
+			
+			addi t0,t0,4
+			addi t1,t1,4
+			
+			addi t3,t3,4
+			blt t3,t4, PRINT_LINHA
+			
+			addi t0,t0,320
+			sub t0,t0,t4
+			
+			 mv t3,zero
+			 addi t2,t2,1
+			 
+			  ble t2,t5,PRINT_LINHA
+			  
+			  ret 
+			
+
+
+
+
